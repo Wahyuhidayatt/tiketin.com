dc3f015156dd656ff0feecdfcb09dba39245159c
wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-04-07 14:22:17 +0700

    testing mocha

--- server/test/testing.js
+++ server/test/testing.js
@@ -5,7 +5,7 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Login testing', function() {
+describe('testing', function() {
   it('Should success if credential is valid', function(done) {
     chai.request('http://localhost:3000')
     .post('/api/login')
@@ -15,14 +15,53 @@ describe('Login testing', function() {
       done()
     })
   });
-});
-describe('testing POST transaction ', function() {
-  it('Harus berhasil masuk ke database', function(done) {
+  it('testing register', function(done) {
+    chai.request('http://localhost:3000')
+    .post('/api/register')
+    .send({username : "wahyu"})
+    .end(function (err, res) {
+      // res.should.have.status(200)
+      res.should.have.be.a('object')
+      // res.body.username.should.be.equal.to("wahyu")
+      done()
+    })
+  });
+  it('testing method GET user', function(done) {
+    chai.request('http://localhost:3000')
+    .get('/api/user')
+    .end(function (err, res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body[0].should.have.property('username');
+      res.body[0].should.have.property('email');
+      res.body[0].should.have.property('password');
+      done()
+    })
+  });
+  it('testing methohd POST transaction', function(done) {
+    let transaction = {
+
+    }
     chai.request('http://localhost:3000')
     .post('/api/transaction')
-    .send({ username: 'wahyu', password: '12345' })
     .end(function (err, res) {
       res.should.have.status(200)
+      res.should.be.json;
+      res.body.should.be.a('object');
+      done()
+    })
+  });
+  it('testing method GET user', function(done) {
+    chai.request('http://localhost:3000')
+    .get('/api/transaction')
+    .end(function (err, res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body[0].should.have.property('userId');
+      res.body[0].should.have.property('tanggalPemesanan');
+      res.body[0].should.have.property('tickets');
       done()
     })
   });
